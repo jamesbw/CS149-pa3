b3d1589fd59da0c5c61d4b90122c203176ee85af
James Whitbeck <james.b.whitbeck@gmail.com>
2013-02-13 10:22:12 -0800

    added imports

--- ArticleInputFormat.java
+++ ArticleInputFormat.java
@@ -1,3 +1,17 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.conf.Configuration;
+ 
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
 public class ArticleInputFormat extends
     FileInputFormat<Text, Text> {
 
