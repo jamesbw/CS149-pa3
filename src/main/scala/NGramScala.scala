26c397091b676200087b4763faaf610d05750501
James Whitbeck <james.b.whitbeck@gmail.com>
2013-02-18 22:34:05 -0800

    more scala"

--- src/main/scala/NGramScala.scala
+++ src/main/scala/NGramScala.scala
@@ -4,7 +4,7 @@
 import spark.SparkContext
 import SparkContext._
 import org.apache.hadoop.io.Text
-// import scala.collection.mutable.PriorityQueue
+import scala.collection.mutable.PriorityQueue
 import scala.collection.mutable.Queue
 import java.io._
 
@@ -67,64 +67,88 @@ object NGramScala extends Application {
 }
 */
 
-object NGramScala {
+object NGramScala extends Serializable{
     case class Article(title: String, score: Int){}
 
-    implicit def articleOrdering: Ordering[Article] = new Ordering[Article] {
+    def articleOrdering = new math.Ordering[Article] {
         def compare(art1: Article, art2: Article): Int = {
             if (art1.score < art2.score)
-                return 1
-            if (art1.score > art2.score)
                 return -1
-            return - art1.title.compareTo(art2.title)
+            if (art1.score > art2.score)
+                return +1
+            return art1.title.compareTo(art2.title)
         }
     }
 
     // def aggregateIntoQueue(queue: PriorityQueue[Article], article: Article ) = {
+    //     if (queue.size < 20) {
+    //         queue.enqueue(article)
+    //     }
+    //     else {
+    //         val minArticle = queue.head
+    //         if (articleOrdering.compare(article,minArticle) > 0){
+    //             queue.enqueue(article)
+    //             queue.dequeue
+    //         }
+    //     }
+    //     queue
+    // }
     def aggregateIntoQueue(queue: Queue[Article], article: Article ) = {
+        var retQueue: Queue[Article] = queue
         if (queue.size < 20) {
-            queue.enqueue(article)
+            retQueue.enqueue(article)
+            retQueue = retQueue.sorted(articleOrdering).toQueue
         }
         else {
-            val minArticle = queue.head
+            val minArticle = retQueue.head
             if (articleOrdering.compare(article,minArticle) > 0){
-                queue.enqueue(article)
+                retQueue.enqueue(article)
 
-                queue.sorted(articleOrdering)
+                retQueue = retQueue.sorted(articleOrdering).toQueue
 
-                queue.dequeue
+                retQueue.dequeue
             }
         }
-        queue
+        retQueue
     }
 
     // def mergeQueues(queue1: PriorityQueue[Article], queue2: PriorityQueue[Article]) = {
+    //     queue2 foreach { article => 
+    //         aggregateIntoQueue(queue1, article)
+    //     }
+    //     queue1
+    // }
     def mergeQueues(queue1: Queue[Article], queue2: Queue[Article]) = {
+        var retQueue: Queue[Article] = queue1
         queue2 foreach { article => 
-            aggregateIntoQueue(queue1, article)
+            retQueue = aggregateIntoQueue(retQueue, article)
         }
-        queue1
+        retQueue
     }
-
-
 
     def main(args: Array[String]) {
         val logFile = "./log" // Should be some file on your system
-        val sc = new SparkContext("local", "Simple Job")
-        // val wiki = sc.hadoopFile[Text, Text, ArticleInputFormat]("/usr/class/cs149/wikipedia/1gb")
-        val wiki = sc.hadoopFile[Text, Text, ArticleInputFormat]("hdfs://myth1:47412/user/jamesbw/input/chunk_aa")
+        // val sc = new SparkContext("local", "Simple Job")
+        val wiki = sc.hadoopFile[Text, Text, ArticleInputFormat]("/usr/class/cs149/wikipedia/1gb")
+
+        // val queue = new PriorityQueue[Article]()(articleOrdering)
+
+        // val wiki = sc.hadoopFile[Text, Text, ArticleInputFormat]("hdfs://myth1:47412/user/jamesbw/input/chunk_aa")
         val query1 = sc.textFile("hdfs://myth1:47412/user/jamesbw/query/query1.txt")
-        val query1str = query1.reduce( _ + _ )
+        val query1str = query1.reduce( _ + "\n" + _ )
         val queryBag = new NGramBag(query1str, 4)
-        val res = wiki.map{ case (title, article) => Article(title.toString, new NGramBag(article.toString, 4).score(queryBag))}.filter(  _.score > 0)
-
-        val queue = new Queue[Article]()
-
-        res.aggregate[Queue[Article]](new Queue[Article]())(aggregateIntoQueue, mergeQueues)
+        val res = wiki.map{ case (title, article) => Article(title.toString, new NGramBag(article.toString, 4).score(queryBag))}
+                      .filter(  _.score > 0)
+        // res.aggregate[PriorityQueue[Article]](queue)(aggregateIntoQueue, mergeQueues)
+        
+        var queue: Queue[Article] = res.aggregate[Queue[Article]](new Queue[Article]())(aggregateIntoQueue, mergeQueues)
+        queue = queue.sorted(articleOrdering).toQueue
 
         val saveStr = queue.toList.reverse.map{ article =>
             "Title: %s ; Score: %d\n" format (article.title, article.score) 
         }.mkString
+
+        println(queue)
 
         val pw = new PrintWriter(new File("./scala_output"))
         pw.write(saveStr)
@@ -132,7 +156,7 @@ object NGramScala {
     }
 }
 
-
+// NGramScala.main(Array())
 
 
 
